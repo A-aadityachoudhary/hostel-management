5b17300950f12e8a51def9ae5a112520cbcbb704
Aaditya Choudhary <aadityach812@gmail.com>
2026-06-06 17:54:45 +0530

    migratoins & associations

--- app/models/allocation.rb
+++ app/models/allocation.rb
@@ -1,4 +1,5 @@
 class Allocation < ApplicationRecord
-  belongs_to :student
+  belongs_to :student, class_name: 'User'
   belongs_to :room
+  enum status: { active: 0, past: 1 }
 end
---
+++ db/schema.rb
@@ -0,0 +1,55 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.1].define(version: 2026_06_06_122033) do
+  create_table "allocations", force: :cascade do |t|
+    t.date "check_in_date"
+    t.date "check_out_date"
+    t.datetime "created_at", null: false
+    t.integer "room_id", null: false
+    t.integer "status"
+    t.integer "student_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_allocations_on_room_id"
+    t.index ["student_id"], name: "index_allocations_on_student_id"
+  end
+
+  create_table "blocks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "location"
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.integer "block_id", null: false
+    t.integer "capacity"
+    t.datetime "created_at", null: false
+    t.integer "floor"
+    t.string "room_number"
+    t.datetime "updated_at", null: false
+    t.index ["block_id"], name: "index_rooms_on_block_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
+    t.string "password_digest"
+    t.integer "role"
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "allocations", "rooms"
+  add_foreign_key "allocations", "students"
+  add_foreign_key "rooms", "blocks"
+end
